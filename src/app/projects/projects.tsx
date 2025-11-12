09c565389af8302d4efcf56fede0cfd8863da75b
khaulanauman <khaulanauman@gmail.com>
2025-11-12 19:21:55 +0500

    added skills and project

--- src/app/projects/projects.tsx
+++ src/app/projects/projects.tsx
@@ -1,13 +1,168 @@
-// Copyright 2025 hp
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+import React from "react";
+
+const projects = () => {
+  return (
+    <div>
+      <section className="projects py-24 relative">
+        <div className="w-full max-w-7xl px-6 lg:px-8 mx-auto">
+          <div className="flex items-center justify-center flex-col gap-5 mb-14">
+            <h2 className="text-3xl font-bold text-black mb-6">My Projects</h2>
+            <div className="bg-orange-100">
+              <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 xl:gap-x-8">
+                  <a href="https://www.asksunnah.com" className="group">
+                    <img
+                      src="\asksunnah.png"
+                      alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
+                      className=" w-fit rounded-lg bg-gray-200 object-cover group-hover:opacity-75 "
+                    />
+                    <p className="mt-4 text-lg font-medium text-gray-900">
+                      Ask Sunnah
+                    </p>
+                    <h3 className="mt-1 text-sm text-gray-700">
+                      A digital platform providing verified answers from Sheikh
+                      Falah Kurkully , grounded in Qur’an and Sunnah.
+                    </h3>
+                  </a>
+                  <a
+                    href="https://www.instagram.com/p/DJG5YWAIbEX/?img_index=1"
+                    className="group"
+                  >
+                    <img
+                      src="\organicxpure.png"
+                      alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
+                      className=" w-fit rounded-lg bg-gray-200 object-cover group-hover:opacity-75 "
+                    />
+                    <p className="mt-4 text-lg font-medium text-gray-900">
+                      OrganicxPure
+                    </p>
+                    <h3 className="mt-1 text-sm text-gray-700">
+                      Website for organicxpure: a brand focused on natural and
+                      organic products.
+                    </h3>
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/posts/devhawks_artificialintelligence-legaltech-innovation-activity-7231657075989241856-uMkD?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEHlaXABgcXpNHMzGq2wB0wwSzATWMzluOM"
+                    className="group"
+                  >
+                    <img
+                      src="\lawlensai.jpg"
+                      alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
+                      className=" w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 "
+                    />
+                    <p className="mt-4 text-lg font-medium text-gray-900">
+                      LawlensAI
+                    </p>
+                    <h3 className="mt-1 text-sm text-gray-700">
+                      An internship project for DevHawks
+                    </h3>
+                  </a>
+                  <a
+                    href="https://devpost.com/software/ra-meow-n?_gl=1*bp6giq*_gcl_au*MjA0MjYwMDc0OS4xNzQ2NTI3NTY3*_ga*OTY2NjAxODI5LjE3NDExNTUwNTk.*_ga_0YHJK3Y10M*czE3NDY5NTUwMjMkbzQkZzEkdDE3NDY5NTUwMzkkajAkbDAkaDA."
+                    className="group"
+                  >
+                    <img
+                      src="/rameown.png"
+                      alt="Person using a pen to cross a task off a productivity paper card."
+                      className=" w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 "
+                    />
+                    <p className="mt-4 text-lg font-medium text-gray-900">
+                      Rameown
+                    </p>
+                    <h3 className="mt-1 text-sm text-gray-700">
+                      A cozy ramen game for the AWS Game Builder Challenge.
+                    </h3>
+                  </a>
+                  <a
+                    href="https://khaulanauman.wixsite.com/bigbrowncookie"
+                    className="group"
+                  >
+                    <img
+                      src="\cookie.jpg"
+                      alt="Hand holding black machined steel mechanical pencil with brass tip and top."
+                      className=" w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 "
+                    />
+                    <p className="mt-4 text-lg font-medium text-gray-900">
+                      Cookie
+                    </p>
+                    <h3 className="mt-1 text-sm text-gray-700">
+                      A website for cookie cafe
+                    </h3>
+                  </a>
+                  <a
+                    href="https://www.figma.com/proto/p054Y9jmP9mOQ544m5HskU/focuslearn?node-id=0-1&t=HQinHkaOm9pFMlr0-1"
+                    className="group"
+                  >
+                    <img
+                      src="\focuslearn.jpg"
+                      alt="Olive drab green insulated bottle with flared screw lid and flat top."
+                      className=" w-fit rounded-lg bg-gray-200  group-hover:opacity-75 "
+                    />
+                    <p className="mt-4 text-lg font-medium text-gray-900">
+                      FocusLearn
+                    </p>
+                    <h3 className="mt-1 text-sm text-gray-700">
+                      Learning platform for neurodivergent learners.
+                    </h3>
+                  </a>
+                  <a
+                    href="https://khaulanauman.github.io/aries/"
+                    className="group"
+                  >
+                    <img
+                      src="/aries.png"
+                      alt="Olive drab green insulated bottle with flared screw lid and flat top."
+                      className=" w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 "
+                    />
+                    <p className="mt-4 text-lg font-medium text-gray-900">
+                      Aries
+                    </p>
+                    <h3 className="mt-1 text-sm text-gray-700">
+                      A website that lets autistic kids learn by playing games
+                      and listening to stories.
+                    </h3>
+                  </a>
+                  <a
+                    href="https://khaulanauman.github.io/football-website/"
+                    className="group"
+                  >
+                    <img
+                      src="\foobi.png"
+                      alt="Person using a pen to cross a task off a productivity paper card."
+                      className="w-fit rounded-lg bg-gray-200 object-fill group-hover:opacity-75 "
+                    />
+                    <p className="mt-4 text-lg font-medium text-gray-900">
+                      Foobi
+                    </p>
+                    <h3 className="mt-1 text-sm text-gray-700">
+                      A wesbite for football fans.
+                    </h3>
+                  </a>
+                  <a
+                    href="https://github.com/pranitha05/EmpowerED/"
+                    className="group"
+                  >
+                    <img
+                      src="\empowered.jpg"
+                      alt="Hand holding black machined steel mechanical pencil with brass tip and top."
+                      className=" w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 "
+                    />
+                    <p className="mt-4 text-lg font-medium text-gray-900">
+                      EmpowerEd
+                    </p>
+                    <h3 className="mt-1 text-sm text-gray-700">
+                      An educational website for underprivileged students with
+                      free courses, community, and gamified learning.
+                    </h3>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default projects;
